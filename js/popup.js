3b9fee6f703f4050e1b7c3e44c7e8bc910b7cbd1
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-10-23 17:03:04 +0800

    feat: up

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,10 @@
 console.log('这是chrome插件')
+chrome.storage.sync.set({ minutes: 1 });
+chrome.storage.local.get('signed_in', (data) => {
+  console.log(data,'kkkkkkkkkkk')
+  if (data.signed_in) {
+    chrome.action.setPopup({popup: 'popup.html'});
+  } else {
+    chrome.action.setPopup({popup: 'popup_sign_in.html'});
+  }
+});
